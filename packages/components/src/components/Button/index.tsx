46e0db64b995e5232160355a992e9ec12630f722
Thinh Au-Tuan <thinhat@local.elarion.com>
2022-04-27 16:26:32 +0700

    Added my project

---
+++ packages/components/src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface Props {
+  onClick: () => void;
+}
+
+const Button: React.FC<Props> = props => {
+  return <button {...props}>Increment</button>;
+};
+
+export default Button;
